c12c54cbddb2fbf76d485cd4925e10f152175fc2
Yassine Taya <57847472+BleedTheFreak@users.noreply.github.com>
2022-04-21 06:39:39 +0000

    Time: 87 ms (66.04%), Space: 48.6 MB (23.66%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.js
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -2,8 +2,8 @@ var isPalindrome = function(s) {
     let arr
     if (s.match(/[a-z0-9]/gi) != null)
     {
-        arr = s.match(/[a-z0-9]/gi).join('').toLowerCase().split('');
-        return arr.every((e,i) => e === arr[arr.length - 1 - i]);
+        arr = [...s.match(/[a-z0-9]/gi)].map(e=>e.toLowerCase())
+        return arr.every((e,i) => e === arr[arr.length - 1 - i])
     }
     return true   
 };
