e5dc5aef5116495a4d7c5183b5af10155758b6a3
Yassine Taya <57847472+BleedTheFreak@users.noreply.github.com>
2022-04-26 04:29:54 +0000

    Time: 1792 ms, Memory: 59.6 MB - LeetHub

--- replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.js
+++ replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.js
@@ -1,11 +1,7 @@
 var replaceElements = function(arr) {
     ret = []
     for (i = 1; i < arr.length ;i++)
-    {
-        let max;
-        if (i < arr.length)  max = Math.max(...arr.slice(i));
-        ret.push(max);
-    }
+        ret.push(Math.max(...arr.slice(i)))
     if (arr.length > 0)
         ret.push(-1)
     return ret;
