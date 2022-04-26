9cc7eaeb461942d6c27ca5154ec1bcb4a2e6582a
Yassine Taya <57847472+BleedTheFreak@users.noreply.github.com>
2022-04-26 04:53:43 +0000

    Time: 57 ms (86.21%), Space: 12.7 MB (19.54%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.c
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.c
@@ -1,23 +1,15 @@
-
-int max(int* arr, int arrSize)
-{
-	int  ret_max = *arr;
-    for (int i = 1 ;i< arrSize;i++)
-        if (ret_max < arr[i])
-            ret_max = arr[i];
-    return ret_max;
-}
-
 int* replaceElements(int* arr, int arrSize, int* returnSize){
     int *ret = (int *) malloc(sizeof(int) * arrSize);
     if (ret)
     {
-        int j = 0;
-        for (int i = 1; i < arrSize;i++)
-            ret[j++] = max(arr + i ,arrSize - i);
+        for (int i = arrSize - 1, max = -1 , tmp ; i >= 0;i--)
+        {
+            tmp = arr[i];
+            ret[i] = max;
+            if (tmp > max)
+                max = tmp;
+        }
         *returnSize = arrSize;
-        if (arrSize >= 1)
-            ret[j] = -1;
         free(arr);
     }
     return ret;
