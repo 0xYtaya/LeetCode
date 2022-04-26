ecd3952811cc68f65a504998309428ddd6a85a46
Yassine Taya <57847472+BleedTheFreak@users.noreply.github.com>
2022-04-26 01:57:20 +0000

    Time: 86 ms, Memory: 44.9 MB - LeetHub

--- remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
+++ remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -1,11 +1,22 @@
-/**
- * @param {number[]} nums
- * @return {number}
- */
+// unsorted array
+
+// var removeDuplicates = function(arr) {
+// 	arrD = []
+// 	for (let i = 0; i < arr.length; i++) {
+// 		if (arrD.indexOf(arr[i]) == -1)
+// 			arrD.push(arr[i]);
+// 	}
+// 	for (let i = 0; i < arrD.length; i++)
+// 		arr[i] = arrD[i]
+// 	return (arrD.length)
+// };
+
+//sorted arry
+
 var removeDuplicates = function(arr) {
 	arrD = []
 	for (let i = 0; i < arr.length; i++) {
-		if (arrD.indexOf(arr[i]) == -1)
+		if (arr[i] !== arr[i + 1])
 			arrD.push(arr[i]);
 	}
 	for (let i = 0; i < arrD.length; i++)
