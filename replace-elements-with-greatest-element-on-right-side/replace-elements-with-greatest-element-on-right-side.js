7724103f7f5c77547038e81070d67977295c9314
Yassine Taya <57847472+BleedTheFreak@users.noreply.github.com>
2022-04-26 04:28:54 +0000

    Time: 1602 ms, Memory: 58.7 MB - LeetHub

--- replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.js
+++ replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.js
@@ -1,24 +1,3 @@
-// int max(int* arr, int arrSize)
-// {
-//     if (arrSize == 1) return -1;
-//     for (int i = 1 , max = arr[0];i< arrSize;i++)
-//         if (max < arr[i])
-//             max = arr[i];
-//     return max;
-// }
-
-// int* replaceElements(int* arr, int arrSize, int* returnSize){
-//     int *ret = malloc(sizeof(int) * arrSize);
-//     if (ret)
-//     {
-//         for (int i = 1 , j = 0; i < arrSize;i++)
-//             ret[j++] = max(arr + i,arrSize - 1);
-//         *returnSize = arrSize;
-//         free(arr);
-//     }
-//     return ret;
-// }
-
 var replaceElements = function(arr) {
     ret = []
     for (i = 1; i < arr.length ;i++)
