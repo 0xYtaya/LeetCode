4a5dfbc2afefd638ca051f712db6fae11ba6063a
Yassine Taya <57847472+BleedTheFreak@users.noreply.github.com>
2022-05-29 01:23:11 +0100

    Time: 128 ms (26.78%), Space: 45 MB (77.60%) - LeetHub

--- 268-missing-number/268-missing-number.ts
+++ 268-missing-number/268-missing-number.ts
@@ -1,5 +1,4 @@
 function missingNumber(nums: number[]): number {
-  let total: number = (((nums.length - 1) + 1) * ((nums.length - 1) + 2)) / 2;
-  let missing: number = nums.reduce((a, e) => a - e, total);
-  return missing;
+  let total: number = ((nums.length - 1 + 1) * (nums.length - 1 + 2)) / 2;
+  return nums.reduce((a, e) => a - e, total);
 }
