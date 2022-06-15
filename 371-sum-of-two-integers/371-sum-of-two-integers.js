580eea183f6677bd7665be2e03ae0aad3f89ff1e
Yassine Taya <57847472+BleedTheFreak@users.noreply.github.com>
2022-06-15 12:32:38 +0100

    Time: 65 ms (80.75%), Space: 42.1 MB (25.67%) - LeetHub

---
+++ 371-sum-of-two-integers/371-sum-of-two-integers.js
@@ -0,0 +1,11 @@
+var getSum = function(a, b) {
+    let carry = 1;
+    
+    while (carry != 0)
+    {
+        carry = a & b;
+        a = a ^ b;
+        b = carry << 1;
+    }
+    return a;
+};
